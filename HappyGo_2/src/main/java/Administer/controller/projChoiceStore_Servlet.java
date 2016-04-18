c0afb0e8c6e2b8ede238614b57095b3b6946e32b
Michael <sparrow1002@gmail.com>
2016-04-18 11:38:40 +0800

    update projChoiceStoreServlet

--- HappyGo_2/src/main/java/Administer/controller/projChoiceStore_Servlet.java
+++ HappyGo_2/src/main/java/Administer/controller/projChoiceStore_Servlet.java
@@ -1,6 +1,7 @@
 package Administer.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.ProductBean;
+import Administer.model.HG_PromotionBonus_Bean;
+import conStore.model.PProjectStoreBean;
 
 @WebServlet(
 		urlPatterns={"/PromotionProject/PromotionStore.controller"}
@@ -25,58 +27,66 @@ public class projChoiceStore_Servlet extends HttpServlet {
 		//思考如何接多筆name=${row.cos_storeid}
 		String PTP_PROJID = request.getParameter("PTP_PROJID");
 		
-		String prodaction = request.getParameter("prodaction");
+		//cos_storeid的資料，由於可能會有1~多筆，必須用while迴圈+陣列處理
+		int j=1;
+		while(request.getParameter("cos_storeid"+j)!=null){
+			j++;
+		}
+		String cos_storeid[] = new String[j];
+		int i =0;
+		while(request.getParameter("cos_storeid"+i)!=null){
+			cos_storeid[i]=request.getParameter("cos_storeid"+i);
+			i++;
+		}
+		
+		String proChoiceStore = request.getParameter("proChoiceStore");
 		
 		//轉換HTML Form資料
-		//不太需要轉換，因為都是勾選+submit
+		Map<String, String> error = new HashMap<String, String>();
+		request.setAttribute("error", error);
+		
+		int int_PTP_PROJID = 0;
+		int_PTP_PROJID = Integer.parseInt(PTP_PROJID.trim());
 		
 		//驗證HTML Form資料
 		//都沒選擇店家，要提醒使用者選擇
+		if("Insert".equals(proChoiceStore)){
+			if(cos_storeid[0]==null) {
+			error.put("noStore", "請至少選擇一個店家");
+			}
+		}
+		
+		//有error，回選擇店家頁面
 		if(error!=null && !error.isEmpty()) {
 			request.getRequestDispatcher(
-					"/pages/product.jsp").forward(request, response);
+					"/Administer/PromotionProject/projChoiceStore.jsp").forward(request, response);
 			return;
 		}
 		
 		//呼叫Model
-		//都沒選擇店家，回到projChoiceStore
-		//有選擇一家以上，回到首頁，告知success
-		if("Select".equals(prodaction)) {
-			List<ProductBean> result = productService.select(bean);
-			request.setAttribute("select", result);
+		List<PProjectStoreBean> beanlist = new ArrayList<>();
+		for(i=0; i<j;i++){
+		PProjectStoreBean bean = new PProjectStoreBean();
+		bean.setPps_projid(PTP_PROJID);
+		bean.setPps_storeid(cos_storeid[i]);
+		beanlist.add(bean);
+		}
+		
+		//根據Model執行結果顯示View
+		
+		if(proChoiceStore!=null && proChoiceStore.equals("Insert")) {
+//			proChoiceStore result = service.insert(beanlist);
+//			if(result==null) {
+//				error.put("action", "Insert fail");
+//			} else {
+//				request.setAttribute("insert", result);
+//			}
 			request.getRequestDispatcher(
-					"/pages/display.jsp").forward(request, response);
-		} else if(prodaction!=null && prodaction.equals("Insert")) {
-			ProductBean result = productService.insert(bean);
-			if(result==null) {
-				error.put("action", "Insert fail");
-			} else {
-				request.setAttribute("insert", result);
-			}
+					"/Administer/PromotionProject/index.jsp").forward(request, response);
+		} else {
+			error.put("action", "Unknown Action:"+proChoiceStore);
 			request.getRequestDispatcher(
-					"/pages/product.jsp").forward(request, response);
-		} else if(prodaction!=null && prodaction.equals("Update")) {
-			ProductBean result = productService.update(bean);
-			if(result==null) {
-				error.put("action", "Update fail");
-			} else {
-				request.setAttribute("update", result);
-			}
-			request.getRequestDispatcher(
-					"/pages/product.jsp").forward(request, response);
-		} else if(prodaction!=null && prodaction.equals("Delete")) {
-			boolean result = productService.delete(bean);
-			if(!result) {
-				request.setAttribute("delete", 0);
-			} else {
-				request.setAttribute("delete", 1);
-			}
-			request.getRequestDispatcher(
-					"/pages/product.jsp").forward(request, response);
-		} else  {
-			error.put("action", "Unknown Action:"+prodaction);
-			request.getRequestDispatcher(
-					"/pages/product.jsp").forward(request, response);
+					"/Administer/PromotionProject/index.jsp").forward(request, response);
 		}
 	}
 
