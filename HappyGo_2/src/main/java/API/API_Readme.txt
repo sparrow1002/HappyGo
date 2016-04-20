d240b629b220bfe87cf7f2647c4c7e18ae7f33e8
Lin Juang-Bao <moelin2007@hotmail.com>
2016-04-20 15:30:55 +0800

    由RESTful改成RESTeasy

--- HappyGo_2/src/main/java/API/API_Readme.txt
+++ HappyGo_2/src/main/java/API/API_Readme.txt
@@ -1,4 +1,4 @@
-/API
+
 	/points 				----transactionAPI()----交易API
 		 	  ?memberId=	----totalPoint()----總點數
 		 	  /點數			----calculateDiscount()----點數可以折多少錢
--- HappyGo_2/src/main/java/API/RESTfulConfig.java
+++ HappyGo_2/src/main/java/API/RESTfulConfig.java
@@ -2,11 +2,11 @@ package API;
 
 import javax.ws.rs.ApplicationPath;
 
-import org.glassfish.jersey.server.ResourceConfig;
+//import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("/API")
-public class RESTfulConfig extends ResourceConfig {
+//@ApplicationPath("/API")
+public class RESTfulConfig /*extends ResourceConfig */{
 	public RESTfulConfig(){
-		this.packages("API","cardPoint.model");
+		//this.packages("API","cardPoint.model");
 	}
 }
